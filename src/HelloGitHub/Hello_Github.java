f2ac208e65529b2536e5d9fc61753365a4e58404
mikewang928 <55117208+mikewang928@users.noreply.github.com>
2020-08-27 17:10:37 +0800

    add version 3.0

--- src/HelloGitHub/Hello_Github.java
+++ src/HelloGitHub/Hello_Github.java
@@ -4,5 +4,6 @@ public class Hello_Github {
     public static void main(String[] args){
         System.out.println("Hello GitHub");
         System.out.println("Guanglun Tech. Inc. is awesome");
+        System.out.println("It's an honor to work with everyone of you");
     }
 }
