51ba560b65f35817cc0bd5581fac0ad9935ca194
mikewang928 <55117208+mikewang928@users.noreply.github.com>
2020-08-27 17:00:15 +0800

    add Guanglun Tech. Inc. is awesome

--- src/HelloGitHub/Hello_Github.java
+++ src/HelloGitHub/Hello_Github.java
@@ -3,5 +3,6 @@ package HelloGitHub;
 public class Hello_Github {
     public static void main(String[] args){
         System.out.println("Hello GitHub");
+        System.out.println("Guanglun Tech. Inc. is awesome");
     }
 }
